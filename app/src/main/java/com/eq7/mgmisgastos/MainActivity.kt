f59e25dbe3e8b7b8d5eea7638cbe53f61c8946a2
DagonNR <danielnarvaezpp@outlook.com>
2026-02-10 13:30:47 -0600

    dev create

--- app/src/main/java/com/eq7/mgmisgastos/MainActivity.kt
+++ app/src/main/java/com/eq7/mgmisgastos/MainActivity.kt
@@ -10,32 +10,4 @@ import com.google.firebase.firestore.FieldValue
 class MainActivity : AppCompatActivity() {
 
     private val db = FirebaseFirestore.getInstance()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val btn = findViewById<Button>(R.id.btnTest)
-        val tv = findViewById<TextView>(R.id.tvResult)
-
-        btn.setOnClickListener {
-            val data = hashMapOf(
-                "status" to "ok",
-                "createdAt" to FieldValue.serverTimestamp()
-            )
-
-            db.collection("ping")
-                .add(data)
-                .addOnSuccessListener { doc ->
-                    tv.text = "Conectado. Doc: ${doc.id}"
-
-                    doc.get().addOnSuccessListener { snap ->
-                        tv.append("\nLeído: ${snap.data}")
-                    }
-                }
-                .addOnFailureListener { e ->
-                    tv.text = "Error: ${e.message}"
-                }
-        }
-    }
 }
