5032697089e7896734bab10ec3a4bf40cf42dd23
DagonNR <danielnarvaezpp@outlook.com>
2026-02-10 13:31:34 -0600

    dev create

--- app/src/main/java/com/eq7/mgmisgastos/MainActivity.kt
+++ app/src/main/java/com/eq7/mgmisgastos/MainActivity.kt
@@ -1,11 +1,7 @@
 package com.eq7.mgmisgastos
 
-import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.FieldValue
 
 class MainActivity : AppCompatActivity() {
 
